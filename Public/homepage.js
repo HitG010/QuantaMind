4d663d8a8d80fd01cb3773ebe2ae98d42eb1d4ea
HitG010 <hiteshgupta_23ec091@dtu.ac.in>
2024-02-18 23:58:18 +0530

    Bug Fixes made by kartik

--- Public/homepage.js
+++ Public/homepage.js
@@ -1,20 +1,7 @@
-let btn = document.querySelector(".emotions button");
-
-btn.addEventListener("click", function () {
-  console.log("you clicked a button");
-  window.location = "http://127.0.0.1:5000/";
-});
-
-let themeBtn = document.querySelector(".themeBtn");
-
-function darkMode() {
-  let body = document.querySelector("body");
-  body.classList.add("dark-bg");
-  document.querySelector("title-upper").classList.add("dark-title");
-  document.querySelector("title-lower").classList.add("dark-title");
-}
-
-themeBtn.addEventListener("click", darkMode);
+// btn.addEventListener("click", function () {
+//   console.log("you clicked a button");
+//   window.location = "http://127.0.0.1:5000/";
+// });
 
 //adding buttons functionality for redirection of pages
 let questionnaireBtn = document.querySelector(".pink-arrow");
@@ -46,5 +33,3 @@ let challengesBtn = document.querySelector(".yellow-arrow");
 challengesBtn.addEventListener("click", function () {
   window.location.href = "/dailyChallenges";
 });
-
-
---
+++ journal/app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template, request 
+from transformers import pipeline, RobertaTokenizerFast
+ 
+emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
+
+app = Flask(__name__,template_folder="templates") 
+
+@app.route("/") 
+def hello(): 
+	return render_template('index.html') 
+
+@app.route('/emotions', methods=['POST']) 
+def process(): 
+	data = request.form.get('data') 
+	result = emotion(data) 
+	return result[0]
+	# return "Hello"
+
+if __name__ == '__main__': 
+	app.run(debug=True) 
